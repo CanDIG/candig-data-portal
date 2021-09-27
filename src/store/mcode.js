ea2b56f5b77554575b1a501376d3994e4809ed33
Jimmy Li <jimli@bcgsc.ca>
2021-09-27 15:12:20 -0700

    update mcode table (#2)
    * update mcode table
    
    * fix function

---
+++ src/store/mcode.js
@@ -0,0 +1,133 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+function moreInfoButton(params) {
+    return (
+        <strong>
+            <Button
+                onClick={() => {
+                    console.log(params.row.id);
+                }}
+            >
+                More Info
+            </Button>
+        </strong>
+    );
+}
+
+export const mainColumns = [
+    {
+        field: 'id',
+        headerName: 'ID',
+        width: 150
+    },
+    {
+        field: 'ethnicity',
+        headerName: 'Ethnicity',
+        width: 150
+    },
+    {
+        field: 'sex',
+        headerName: 'Sex',
+        width: 150
+    },
+    {
+        field: 'more_info',
+        headerName: 'Info',
+        width: 100,
+        sortable: false,
+        headerAlign: 'center',
+        align: 'center',
+        renderCell: moreInfoButton,
+        disableClickEventBubbling: true
+    }
+];
+
+export const subjectColumns = [
+    {
+        field: 'id',
+        headerName: 'ID',
+        width: 150
+    },
+    {
+        field: 'date_of_birth',
+        headerName: 'Date of Birth',
+        width: 150
+    },
+    {
+        field: 'date_of_death',
+        headerName: 'Date of Death',
+        width: 150
+    },
+    {
+        field: 'ethnicity',
+        headerName: 'Ethnicity',
+        width: 150
+    },
+    {
+        field: 'sex',
+        headerName: 'Sex',
+        width: 150
+    },
+    {
+        field: 'Administrative Gender',
+        headerName: 'administrative_gender',
+        width: 150
+    },
+    {
+        field: 'Language',
+        headerName: 'communication_language',
+        width: 150
+    }
+];
+
+export const cancerRelatedProceduresColumns = [
+    {
+        field: 'id',
+        headerName: 'id',
+        width: 150
+    },
+    {
+        field: 'procedure_type',
+        headerName: 'Procedure Type',
+        width: 150
+    },
+    {
+        field: 'procedure_code_id',
+        headerName: 'Code ID',
+        width: 150
+    },
+    {
+        field: 'procedure_code_label',
+        headerName: 'Code Label',
+        width: 150
+    },
+    {
+        field: 'body_site_id',
+        headerName: 'Body Site ID',
+        width: 150
+    },
+    {
+        field: 'body_site_label',
+        headerName: 'Body Site Label',
+        width: 150
+    }
+];
+
+export const medicationStatementColumns = [
+    {
+        field: 'id',
+        headerName: 'id',
+        width: 150
+    },
+    {
+        field: 'medication_code_id',
+        headerName: 'Medication Code ID',
+        width: 150
+    },
+    {
+        field: 'medication_code_label',
+        headerName: 'Medication Code Label',
+        width: 150
+    }
+];
--- src/views/sample-page/index.js
+++ src/views/sample-page/index.js
@@ -1,61 +1,52 @@
 import React from 'react';
 
 // material-ui
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 
 // project imports
 import MainCard from 'ui-component/cards/MainCard';
+import { katsu } from 'store/api';
+import { mainColumns } from 'store/mcode';
 
-//= =============================|| SAMPLE PAGE ||==============================//
-const columns = [
-    { field: 'id', headerName: 'ID', width: 200 },
-    {
-        field: 'firstName',
-        headerName: 'First name',
-        width: 200,
-        editable: true
-    },
-    {
-        field: 'lastName',
-        headerName: 'Last name',
-        width: 200,
-        editable: true
-    },
-    {
-        field: 'age',
-        headerName: 'Age',
-        type: 'number',
-        width: 110,
-        editable: true
-    },
-    {
-        field: 'fullName',
-        headerName: 'Full name',
-        description: 'This column has a value getter and is not sortable.',
-        sortable: false,
-        width: 160,
-        valueGetter: (params) => `${params.getValue(params.id, 'firstName') || ''} ${params.getValue(params.id, 'lastName') || ''}`
+function MCodeView() {
+    const [rows, setRows] = React.useState([]);
+
+    function processMCodeMainData(dataObject) {
+        const row = {};
+
+        row.id = dataObject.id;
+        row.sex = dataObject.subject.sex;
+        row.ethnicity = dataObject.subject.ethnicity;
+
+        return row;
     }
-];
 
-const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 200 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 }
-];
+    React.useEffect(() => {
+        fetch(`${katsu}/api/mcodepackets`)
+            .then((response) => response.json())
+            .then((data) => {
+                const tempRows = [];
+                for (let i = 0; i < data.results.length; i += 1) {
+                    tempRows.push(processMCodeMainData(data.results[i]));
+                }
+                setRows(tempRows);
+            });
+    }, []);
 
-const SamplePage = () => (
-    <MainCard title="Sample Card">
-        <div style={{ height: 600, width: '100%' }}>
-            <DataGrid rows={rows} columns={columns} pageSize={10} rowsPerPageOptions={[10]} checkboxSelection disableSelectionOnClick />
-        </div>
-    </MainCard>
-);
+    return (
+        <MainCard title="mCode Data">
+            <div style={{ height: 600, width: '40%' }}>
+                <DataGrid
+                    rows={rows}
+                    columns={mainColumns}
+                    pageSize={10}
+                    rowsPerPageOptions={[10]}
+                    components={{ Toolbar: GridToolbar }}
+                    disableSelectionOnClick
+                />
+            </div>
+        </MainCard>
+    );
+}
 
-export default SamplePage;
+export default MCodeView;
