5404522a5053ddaed0810164bcc3a5306b78b207
Jimmy Li <lihaoyuan1996@gmail.com>
2022-03-25 11:58:48 -0700

    make lightcard reusable

--- src/views/dashboard/Default/LightCard.js
+++ src/views/dashboard/Default/LightCard.js
@@ -9,54 +9,58 @@ import { Avatar, List, ListItem, ListItemAvatar, ListItemText, Typography } from
 import MainCard from 'ui-component/cards/MainCard';
 import TotalIncomeCard from 'ui-component/cards/Skeleton/TotalIncomeCard';
 
-// style constant
-const useStyles = makeStyles((theme) => ({
-    card: {
-        overflow: 'hidden',
-        position: 'relative',
-        '&:after': {
-            content: '""',
-            position: 'absolute',
-            width: '210px',
-            height: '210px',
-            background: `linear-gradient(210.04deg, ${theme.palette.grey[700]} -50.94%, rgba(144, 202, 249, 0) 83.49%)`,
-            borderRadius: '50%',
-            top: '-30px',
-            right: '-180px'
-        },
-        '&:before': {
-            content: '""',
-            position: 'absolute',
-            width: '210px',
-            height: '210px',
-            background: `linear-gradient(140.9deg, ${theme.palette.grey[700]} -14.02%, rgba(144, 202, 249, 0) 70.50%)`,
-            borderRadius: '50%',
-            top: '-160px',
-            right: '-130px'
-        }
-    },
-    content: {
-        padding: '16px !important'
-    },
-    avatar: {
-        ...theme.typography.commonAvatar,
-        ...theme.typography.largeAvatar,
-        backgroundColor: theme.palette.grey[300],
-        color: theme.palette.grey[700]
-    },
-    secondary: {
-        color: theme.palette.grey[500],
-        marginTop: '5px'
-    },
-    padding: {
-        paddingTop: 0,
-        paddingBottom: 0
-    }
-}));
-
 // ===========================|| DASHBOARD - TOTAL INCOME LIGHT CARD ||=========================== //
 
-const LightCard = ({ isLoading, header, value, icon }) => {
+const LightCard = ({ isLoading, header, value, icon, color = 'grey' }) => {
+    const useStyles = makeStyles((theme) => ({
+        card: {
+            backgroundColor: '#fff',
+            color: theme.palette[color].light,
+            overflow: 'hidden',
+            position: 'relative',
+            '&:after': {
+                content: '""',
+                position: 'absolute',
+                width: '210px',
+                height: '210px',
+                background: `linear-gradient(210.04deg, ${theme.palette[color].dark} -50.94%, rgba(144, 202, 249, 0) 83.49%)`,
+                borderRadius: '50%',
+                top: '-30px',
+                right: '-180px'
+            },
+            '&:before': {
+                content: '""',
+                position: 'absolute',
+                width: '210px',
+                height: '210px',
+                background: `linear-gradient(140.9deg, ${theme.palette[color].dark} -14.02%, rgba(144, 202, 249, 0) 70.50%)`,
+                borderRadius: '50%',
+                top: '-160px',
+                right: '-130px'
+            }
+        },
+        content: {
+            padding: '16px !important'
+        },
+        avatar: {
+            ...theme.typography.commonAvatar,
+            ...theme.typography.largeAvatar,
+            backgroundColor: theme.palette[color].dark,
+            color: '#fff'
+        },
+        primary: {
+            color: '#fff'
+        },
+        secondary: {
+            color: 'black',
+            marginTop: '5px'
+        },
+        padding: {
+            paddingTop: 0,
+            paddingBottom: 0
+        }
+    }));
+
     const classes = useStyles();
 
     return (
