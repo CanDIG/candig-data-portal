f997d00a55ed68f5d4e5207c60f0d91ae7a747d8
Courtney Gosselin <cgosselin@bcgsc.ca>
2022-03-29 15:54:30 -0700

    Add more images
    Signed-off-by: Courtney Gosselin <cgosselin@bcgsc.ca>

--- src/views/errorPages/ErrorNotFoundPage.js
+++ src/views/errorPages/ErrorNotFoundPage.js
@@ -7,15 +7,21 @@ import Typography from '@mui/material/Typography';
 
 // project imports
 import MainCard from 'ui-component/cards/MainCard';
-import Error404 from 'assets/images/Error404.svg';
+import Error4040 from 'assets/images/ErrorPages/Error404-0.svg';
+import Error4041 from 'assets/images/ErrorPages/Error404-1.svg';
+import Error4042 from 'assets/images/ErrorPages/Error404-2.svg';
+import Error4043 from 'assets/images/ErrorPages/Error404-3.svg';
 
 function ErrorNotFoundPage() {
     const theme = useTheme();
 
+    const errorImages = [Error4040, Error4041, Error4042, Error4043];
+    const index = Math.floor(Math.random() * errorImages.length);
+
     return (
         <MainCard title="Error 404: Page Not Found" justifyContent="center" alignItems="center" sx={{ minHeight: 830 }}>
             <Grid container direction="column" justifyContent="center" alignItems="center" sx={{ minHeight: 200, padding: 10 }}>
-                <img src={Error404} alt="Error 404" width="500" />
+                <img src={errorImages[index]} alt="Error 404" width="500" />
                 <Typography variant="h4" gutterBottom component="div" m={4}>
                     Sorry, there was a problem with your request. You can redirect to the Overview page below or choose from the sidebar on
                     the left.
