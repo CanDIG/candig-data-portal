731c795de459e660183a891ff1ed64112f323679
Jimmy Li <lihaoyuan1996@gmail.com>
2022-05-16 08:05:12 -0700

    initial commit

---
+++ src/ui-component/IGV/HtsgetInstance.js
@@ -0,0 +1,85 @@
+import React, { useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+import igv from '@candig/igv/dist/igv.esm';
+import { htsget } from 'store/api';
+
+function HtsgetInstance(BamIdList, VcfIdList) {
+    /** *
+     * A functional component that returns an IGV.js instance dedicated to rendering GWAS data.
+     * Note: There does not seem to be a way for us to know which reference genome is being used
+     * Therefore, the Htsget instance below defaults to using hg38
+     */
+    const igvBrowser = useRef(null);
+
+    const modelBamTrack = {
+        type: 'alignment',
+        sourceType: 'htsget',
+        format: 'bam',
+        url: `${htsget}/htsget/v1/reads/data/`
+    };
+
+    const modelVcfTrack = {
+        type: 'variant',
+        format: 'vcf',
+        sourceType: 'htsget',
+        url: `${htsget}/htsget/v1/variants/data/`
+    };
+
+    // Dynamically create a list of tracks based on types of files available
+    // Example tracks are listed as below
+
+    // {
+    //     type: 'alignment',
+    //     sourceType: 'htsget',
+    //     format: 'bam',
+    //     url: `${htsget}/htsget/v1/reads/data/NA02102.bam`
+    // },
+    // {
+    //     type: 'variant',
+    //     format: 'vcf',
+    //     sourceType: 'htsget',
+    //     url: `${htsget}/htsget/v1/variants/data/NA18537`
+    // }
+
+    useEffect(() => {
+        const igvOptions = {
+            genome: 'hg38',
+            tracks: []
+        };
+
+        const tracks = [];
+
+        // Create a list of tracks based on the list of BAM IDs
+        for (let i = 0; i < BamIdList.length; i += 1) {
+            const newBamTrack = modelBamTrack;
+            newBamTrack.url += BamIdList[i];
+            tracks.push(newBamTrack);
+        }
+
+        // Create a list of tracks based on the list of VCF IDs
+        for (let i = 0; i < VcfIdList.length; i += 1) {
+            const newVcfTrack = modelVcfTrack;
+            newVcfTrack.url += VcfIdList[i];
+            tracks.push(newVcfTrack);
+        }
+
+        // Update the tracks
+        igvOptions.tracks = tracks;
+
+        igv.removeAllBrowsers(); // Remove existing browser instances
+
+        // Do not create new browser instance on page load as no sample is selected.
+        igv.createBrowser(igvBrowser.current, igvOptions);
+    }, [BamIdList, VcfIdList]);
+
+    return (
+        <>
+            <div style={{ background: 'white', marginTop: '15px', marginBottom: '15px' }} ref={igvBrowser} />
+        </>
+    );
+}
+
+HtsgetInstance.propTypes = {};
+
+export default HtsgetInstance;
---
+++ src/views/genomicsData/HtsgetBrowser.js
@@ -0,0 +1,143 @@
+import React, { useState, useEffect } from 'react';
+import { Box, Button, FormControl } from '@mui/material';
+import { Grid } from '@material-ui/core';
+import MainCard from 'ui-component/cards/MainCard';
+
+import { useSelector } from 'react-redux';
+import { MultiSelect } from 'react-multi-select-component';
+import LightCard from 'views/dashboard/Default/LightCard';
+import { Map, Description } from '@material-ui/icons';
+import AlertComponent from 'ui-component/AlertComponent';
+
+import HtsgetInstance from 'ui-component/IGV/HtsgetInstance';
+import { fetchKatsu } from 'store/api';
+
+import { LoadingIndicator, usePromiseTracker, trackPromise } from 'ui-component/LoadingIndicator/LoadingIndicator';
+
+import 'assets/css/VariantsSearch.css';
+
+function HtsgetBrowser() {
+    const [isLoading, setLoading] = useState(true);
+    const events = useSelector((state) => state);
+    const [BamCount, setBamCount] = useState('');
+    const [VcfCount, setVcfCount] = useState('');
+    const { promiseInProgress } = usePromiseTracker();
+    const [options, setOptions] = useState([]);
+    const [selected, setSelected] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [selectedBamIds, setSelectedBamIds] = useState([]);
+    const [selectedVcfIds, setSelectedVcfIds] = useState([]);
+    const [alertMessage, setAlertMessage] = useState('');
+    const [alertSeverity, setAlertSeverity] = useState('');
+
+    useEffect(() => {
+        setSelected([]);
+        options.length = 0;
+        setSelectedBamIds([]);
+        setSelectedVcfIds([]);
+        setLoading(false);
+
+        // Get a list of genomics files available
+        trackPromise(
+            fetchKatsu('/api/genomicsreports')
+                .then((data) => {
+                    // TODO: get a list of ids of genomics files from API above
+                    // Or any other APIs that can provide a list of IDs
+                    // Note: if only VCF files are served, then the following TODOs concerning BAM files
+                    // can all be removed.
+                })
+                .catch(() => {
+                    setBamCount('Not Available');
+                    setVcfCount('Not Available');
+                })
+        );
+    }, [options]);
+
+    const formHandler = (e) => {
+        e.preventDefault(); // Prevent form submission
+
+        // TODO: Dynamically create a list of IDs that have been selected
+        // TODO: And pass this list of IDs into htsgetInstance
+        setSelectedBamIds([]);
+        setSelectedVcfIds([]);
+    };
+
+    return (
+        <>
+            <MainCard title="Htsget Browser" sx={{ minHeight: 830, position: 'relative' }}>
+                <AlertComponent
+                    open={open}
+                    setOpen={setOpen}
+                    text={alertMessage}
+                    severity={alertSeverity}
+                    variant="filled"
+                    fontColor={alertSeverity === 'error' ? 'white' : 'black'}
+                />
+                <Grid container direction="column" className="content">
+                    <Grid container direction="row" justifyContent="center" spacing={2} p={2}>
+                        <Grid item sm={12} xs={12} md={4} lg={4}>
+                            {promiseInProgress === true ? (
+                                <LoadingIndicator />
+                            ) : (
+                                <LightCard
+                                    isLoading={isLoading}
+                                    header="VCFs"
+                                    value={VcfCount}
+                                    icon={<Map fontSize="inherit" />}
+                                    color="primary"
+                                />
+                            )}
+                        </Grid>
+                        <Grid item sm={12} xs={12} md={4} lg={4}>
+                            {promiseInProgress === true ? (
+                                <LoadingIndicator />
+                            ) : (
+                                <LightCard
+                                    isLoading={isLoading}
+                                    header="BAMs"
+                                    value={BamCount}
+                                    icon={<Description fontSize="inherit" />}
+                                    color="secondary"
+                                />
+                            )}
+                        </Grid>
+                    </Grid>
+
+                    <form inline onSubmit={formHandler} style={{ justifyContent: 'center' }}>
+                        <Grid container direction="row" justifyContent="center" alignItems="center" spacing={2} p={2}>
+                            <Grid item>
+                                {options.length > 0 && (
+                                    <FormControl>
+                                        <Grid container direction="row" alignItems="center">
+                                            <Box mr={2}>
+                                                <p>Genomics Files</p>
+                                            </Box>
+                                            <MultiSelect // Width set in CSS
+                                                options={options}
+                                                value={selected}
+                                                onChange={setSelected}
+                                                labelledBy="Genomics Files"
+                                            />
+                                        </Grid>
+                                    </FormControl>
+                                )}
+                            </Grid>
+
+                            <Grid item>
+                                <FormControl>
+                                    <Button type="submit" variant="contained">
+                                        View
+                                    </Button>
+                                </FormControl>
+                            </Grid>
+                        </Grid>
+                    </form>
+
+                    <HtsgetInstance BamIdList={selectedBamIds} VcfIdList={selectedVcfIds} />
+                </Grid>
+            </MainCard>
+        </>
+    );
+}
+
+export default HtsgetBrowser;
