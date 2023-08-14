f9bb73caa5e8da0d2f9e53ca14983e9c68df0337
Justin <justin106@protonmail.com>
2023-08-14 15:14:22 -0400

    Ingest page code style fixes

--- src/ui-component/ingest/ClinicalIngest.js
+++ src/ui-component/ingest/ClinicalIngest.js
@@ -6,6 +6,7 @@ export default function ClinicalIngestPage() {
             sx={{
                 width: '100%',
                 height: '100%',
+                minHeight: 100,
                 borderBottomLeftRadius: '0.75em',
                 borderBottomRightRadius: '0.75em',
                 marginTop: 0,
