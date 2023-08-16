c43bc7403aa98ccfbbbbd37ce781d694bc91a042
Justin <justin106@protonmail.com>
2023-08-16 17:59:13 -0400

    Basic clinical ingest page skeleton

---
+++ src/ui-component/DataRow.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Box, Divider, Grid, Typography } from '@mui/material';
+import { useTheme, makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles((theme) => ({
+    rowText: {
+        color: 'black'
+    },
+    container: {
+        minHeight: 40,
+        marginRight: 0
+    },
+    locked: {
+        backgroundColor: theme.palette.action.disabledBackground
+    },
+    button: {
+        // Right-aligned
+        float: 'right',
+        marginLeft: 'auto'
+    },
+    divider: {
+        borderColor: theme.palette.primary.main,
+        marginTop: '0.25em',
+        marginBottom: '0.25em'
+    },
+    dataBox: {
+        maxHeight: 'none',
+        height: 'fit-content',
+        paddingTop: '0.75em',
+        paddingBottom: '0.75em',
+        marginTop: '3em'
+    },
+    label: {
+        marginTop: '-3.5em',
+        paddingBottom: '1.75em',
+        color: 'black'
+    }
+}));
+
+function DataRow(props) {
+    const { fields, itemSize, rowWidth } = props;
+    const theme = useTheme();
+    const classes = useStyles();
+    const primaryField = fields.shift();
+
+    /* const avatarProps = locked
+        ? {
+              // If we're locked out, gray out the avatar
+              sx: { bgcolor: theme.palette.action.disabled }
+          }
+        : {}; */
+
+    return (
+        <Box
+            className={classes.dataBox}
+            sx={{
+                // These properties aren't working in useStyles for some reason...
+                border: 1,
+                borderRadius: 2,
+                boxShadow: 2,
+                borderColor: theme.palette.primary.main,
+                display: 'flex',
+                width: rowWidth
+            }}
+            pr={2}
+        >
+            <Grid container justifyContent="flex-start" alignItems="center" className={classes.container}>
+                <Grid item xs>
+                    <Typography className={classes.label} align="center">
+                        <b>{primaryField.label}</b>
+                    </Typography>
+                    <Typography align="center" variant="h6" className={classes.rowText} sx={{ fontSize: itemSize }}>
+                        {primaryField.value}
+                    </Typography>
+                </Grid>
+                {fields.map((field, idx) => (
+                    <React.Fragment key={idx}>
+                        <Divider flexItem orientation="vertical" className={classes.divider} />
+                        <Grid item xs>
+                            <Typography className={classes.label} align="center">
+                                <b>{field.label}</b>
+                            </Typography>
+                            <Typography align="center" variant="body1" className={classes.rowText} sx={{ fontSize: itemSize }}>
+                                {field.value}
+                            </Typography>
+                        </Grid>
+                    </React.Fragment>
+                ))}
+            </Grid>
+        </Box>
+    );
+}
+
+DataRow.defaultProps = {
+    itemSize: '16px',
+    rowWidth: '100%'
+};
+
+DataRow.propTypes = {
+    fields: PropTypes.arrayOf(
+        PropTypes.shape({
+            label: PropTypes.string.isRequired,
+            value: PropTypes.string.isRequired
+        })
+    ).isRequired,
+    itemSize: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    rowWidth: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+};
+
+export default DataRow;
--- src/ui-component/ingest/ClinicalIngest.js
+++ src/ui-component/ingest/ClinicalIngest.js
@@ -1,12 +1,90 @@
-import { Button } from '@mui/material';
+import { Button, Grid, TextField, Typography } from '@mui/material';
 import PropTypes from 'prop-types';
+import DataRow from 'ui-component/DataRow';
+import { makeStyles } from '@mui/styles';
 
-const ClinicalIngest = ({ setTab }) => (
-    // setTab should be a function that sets the tab to the genomic ingest page
-    <Button sx={{ position: 'absolute', right: '0.2em', bottom: '0.2em' }} onClick={setTab} variant="contained">
-        Next
-    </Button>
-);
+function makeField(lab, val) {
+    return {
+        label: lab,
+        value: val
+    };
+}
+
+const ClinicalIngest = ({ setTab }) => {
+    const dataRowFields = [
+        [makeField('Cohort', 'MOCK COHORT'), makeField('Clinical Patients', '850'), makeField('Read Access', '3')],
+        [makeField('Cohort', 'MOCK COHORT 2'), makeField('Clinical Patients', '325'), makeField('Read Access', '1')],
+        [makeField('Cohort', 'MOCK COHORT 2'), makeField('Clinical Patients', '78'), makeField('Read Access', '2')]
+    ];
+
+    const useStyles = makeStyles({
+        titleText: {
+            color: 'black',
+            fontSize: '1.5em',
+            fontFamily: 'Roboto'
+        },
+        bodyText: {
+            color: 'black',
+            fontSize: '1em',
+            fontFamily: 'Catamaran'
+        }
+    });
+    const classes = useStyles();
+
+    return (
+        // setTab should be a function that sets the tab to the genomic ingest page
+        <>
+            <Grid container direction="column" sx={{ flexGrow: 1 }} spacing={4}>
+                <Grid item>
+                    <Typography align="left" className={classes.titleText}>
+                        <b>Active cohorts</b>
+                    </Typography>
+                    <Grid direction="row" spacing={3} container>
+                        {dataRowFields.map((fields, index) => (
+                            <Grid item xs={5} key={index}>
+                                <DataRow rowWidth="100%" itemSize="0.9em" fields={fields} />
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Grid>
+                <Grid item>
+                    <Typography align="left" className={classes.titleText}>
+                        <b>Choose a cohort for validation</b>
+                    </Typography>
+                    <Grid container sx={{ marginTop: '0.5em', marginLeft: '1em' }} direction="row" alignItems="center" spacing={2}>
+                        <Grid item>
+                            <Typography align="left" className={classes.bodyText}>
+                                <b>Clinical data:</b>
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <TextField sx={{ width: '100%' }} id="clinical-file" variant="outlined" type="file" />
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item>
+                    <Typography align="left" className={classes.titleText}>
+                        <b>Live Preview Summary</b>
+                    </Typography>
+                    <Typography sx={{ color: 'grey' }} align="left" className={classes.bodyText}>
+                        Waiting for upload...
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <Typography align="left" className={classes.titleText}>
+                        <b>Validation</b>
+                    </Typography>
+                    <Typography sx={{ color: 'grey' }} align="left" className={classes.bodyText}>
+                        Waiting for upload...
+                    </Typography>
+                </Grid>
+            </Grid>
+            <Button sx={{ position: 'absolute', right: '0.2em', bottom: '0.2em' }} onClick={setTab} variant="contained">
+                Next
+            </Button>
+        </>
+    );
+};
 
 ClinicalIngest.propTypes = {
     setTab: PropTypes.func.isRequired
