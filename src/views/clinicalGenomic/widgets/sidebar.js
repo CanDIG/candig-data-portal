ed10a604a211e653230e630ca4591fb563cffce3
Courtney <37649170+CourtneyGosselin@users.noreply.github.com>
2024-08-20 11:04:30 -0700

    Reset Filter button selects all nodes (#170)
    * Reset bug for nodes array
    * Empty objects can now be used to select all
    
    Signed-off-by: Courtney Gosselin <courtney@gosselin.io>

--- src/views/clinicalGenomic/widgets/sidebar.js
+++ src/views/clinicalGenomic/widgets/sidebar.js
@@ -428,18 +428,9 @@ function Sidebar() {
 
     function resetButton() {
         // Reset state variables for checkboxes and dropdowns
-        const locations = readerContext?.programs?.map((loc) => loc.location.name);
+        setSelectedNodes({});
+        setSelectedCohorts({});
 
-        if (locations) {
-            const selectedNodes = locations.reduce((acc, loc) => {
-                acc[loc] = true;
-                return acc;
-            }, {});
-
-            setSelectedNodes(selectedNodes);
-        }
-
-        setSelectedCohorts([readerContext?.federation?.map((loc) => loc.results.map((cohort) => cohort.program_id)).flat(1) || []]);
         // Genomic
         setSelectedGenes('');
         setSelectedChromosomes('');
