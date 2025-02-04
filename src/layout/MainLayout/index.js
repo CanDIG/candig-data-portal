289873ea600a05a2d2474d3ae8b171e565fc9fee
fnguyen <OrdinaryNeumann@gmail.com>
2025-02-04 13:14:07 -0500

    Remove logos for DHDP

--- src/layout/MainLayout/index.js
+++ src/layout/MainLayout/index.js
@@ -13,7 +13,7 @@ import clsx from 'clsx';
 // project imports
 import Breadcrumbs from 'ui-component/extended/Breadcrumbs';
 import Header from './Header';
-import Footer from './Footer';
+// import Footer from './Footer';
 import Sidebar from './Sidebar';
 import navigation from 'menu-items';
 import { drawerWidth } from 'store/constant';
@@ -154,7 +154,7 @@ function MainLayout() {
                 </main>
 
                 {/* FOOTER */}
-                <Footer className={leftDrawerOpened ? classes.footerWidth : classes.footer} />
+            {/* <Footer className={leftDrawerOpened ? classes.footerWidth : classes.footer} /> */}
             </SidebarProvider>
         </Root>
     );
