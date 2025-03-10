a22eb39a6e3785b0358c4f56ef00fae5dd34b414
fnguyen <OrdinaryNeumann@gmail.com>
2025-03-10 13:13:01 -0400

    [wip] Rethemeing the top bar

--- src/themes/typography.js
+++ src/themes/typography.js
@@ -1,3 +1,5 @@
+import config from "config";
+
 /**
  * Typography used in theme
  * @param {JsonObject} theme theme customization object
@@ -88,7 +90,7 @@ export default function themeTypography(theme) {
             minHeight: 'calc(100vh - 88px)',
             flexGrow: 1,
             padding: '20px',
-            marginTop: '88px',
+            marginTop: config.isDHDP ? '0px' : '88px',
             marginRight: '20px',
             borderRadius: `${theme.customization.borderRadius}px`
         },
