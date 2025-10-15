0bfba2aef70f5782deac2fb7552ea312d7f32f0e
Courtney <37649170+CourtneyGosselin@users.noreply.github.com>
2025-10-15 12:34:29 -0700

    Feature/genomic data types (#229)
    * Merge clinical and genomic table
    
    Signed-off-by: Courtney Gosselin <courtney@gosselin.io>
    
    * Fix prettier issues
    
    Signed-off-by: Courtney Gosselin <courtney@gosselin.io>
    
    * Adjust Matching Patient Heading
    
    Signed-off-by: Courtney Gosselin <courtney@gosselin.io>
    
    * Merged clinical and genomic table
    
    Signed-off-by: Courtney Gosselin <courtney@gosselin.io>
    
    * Sorting clinical and genomic table
    
    Signed-off-by: Courtney Gosselin <courtney@gosselin.io>
    
    * WIP: File sort
    
    Signed-off-by: Courtney Gosselin <courtney@gosselin.io>
    
    * Fix search explainer
    
    Signed-off-by: Courtney Gosselin <courtney@gosselin.io>
    
    * Adjusting table explainer for search explainer
    
    Signed-off-by: Courtney Gosselin <courtney@gosselin.io>
    
    * Remove console logs
    
    Signed-off-by: Courtney Gosselin <courtney@gosselin.io>
    
    * Fix prettier issues
    
    Signed-off-by: Courtney Gosselin <courtney@gosselin.io>
    
    * Eslint warning resolution
    
    Signed-off-by: Courtney Gosselin <courtney@gosselin.io>
    
    * Set genomic_data_types filter to false intially
    
    Signed-off-by: Courtney Gosselin <courtney@gosselin.io>
    
    * Eslint warning resolution
    
    Signed-off-by: Courtney Gosselin <courtney@gosselin.io>
    
    ---------
    
    Signed-off-by: Courtney Gosselin <courtney@gosselin.io>

--- src/views/clinicalGenomic/clinicalGenomicSearch.js
+++ src/views/clinicalGenomic/clinicalGenomicSearch.js
@@ -7,14 +7,13 @@ import { AppBar, Button, Toolbar, Typography } from '@mui/material';
 import MainCard from 'ui-component/cards/MainCard';
 import PatientCounts from './widgets/patientCounts';
 import DataVisualization from './widgets/dataVisualization';
-import ClinicalData from './widgets/clinicalData';
 import { useSidebarWriterContext } from '../../layout/MainLayout/Sidebar/SidebarContext';
 import Sidebar from './widgets/sidebar';
 import SearchHandler from './search/SearchHandler';
-import GenomicData from './widgets/genomicData';
 import { SearchIndicator } from 'ui-component/LoadingIndicator/SearchIndicator';
 import AuthorizationSections from './widgets/authorizationSections';
 import SearchExplainer from './widgets/searchExplainer';
+import MatchingPatients from './widgets/matchingPatients';
 
 const PREFIX = 'ClinicalGenomicSearch';
 
@@ -115,14 +114,9 @@ const sections = [
         component: <AuthorizationSections title="Authorized Programs" />
     },
     {
-        id: 'clinical',
-        header: 'Clinical Data',
-        component: <ClinicalData />
-    },
-    {
-        id: 'genomic',
-        header: 'Genomic Data',
-        component: <GenomicData />
+        id: 'Matching Patients',
+        header: 'Matching Patients',
+        component: <MatchingPatients />
     }
 ];
 function ClinicalGenomicSearch() {
