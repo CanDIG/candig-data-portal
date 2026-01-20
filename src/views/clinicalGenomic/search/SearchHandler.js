ab1abb5e07d510d5acfc49176cbf175d50a7eb5b
fnguyen <OrdinaryNeumann@gmail.com>
2026-01-20 13:34:42 -0500

    Handle undefined better

--- src/views/clinicalGenomic/search/SearchHandler.js
+++ src/views/clinicalGenomic/search/SearchHandler.js
@@ -67,13 +67,17 @@ function FormatClinicalData(data) {
 
 function FormatFederationData(data) {
     return data.map((site) => {
+        if (!site?.results?.info?.patients_per_program) {
+            return undefined;
+        }
+
         const newResults = Object.keys(site?.results?.info?.patients_per_program).map((program) => ({
             patients_count: site?.results?.info?.patients_per_program[program],
             program_id: program
         }));
 
         return { ...site, results: newResults };
-    });
+    }).filter((item) => item);
 }
 
 function FormatSidebarData(data) {
